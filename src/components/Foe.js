66634db58a7cd36016b3aca27355d17137f6734b
Ann the Cat <lippastik@gmail.com>
2020-06-06 01:59:07 +0300

    HP bars

--- src/components/Foe.js
+++ src/components/Foe.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect, useCallback} from 'react'
 import styled from 'styled-components'
 import FoeImage from '../img/foe.png'
 
@@ -11,10 +11,56 @@ const StyledFoe = styled.div`
   position: absolute;
   right: 175px;
   bottom: 20px;
+
+  .hp-bar {
+    width: 100%;
+    height: 20px;
+    top: -35px;
+    position: absolute;
+    background: red;
+    border: 4px solid #440303;
+    &:before {
+      content: '';
+      position: absolute;
+      display: block;
+      background: #fff;
+      width: ${(props) => 100 - (100 / props.maxHp * props.hp)}%;
+      height: 100%;
+      right: 0;
+    }
+  }
 `
 
 export default function Foe() {
+  const passiveMoney = useCallback(() => {
+    setMoney((newMoney) => (newMoney+1))
+  }, []);
+
+  useEffect(() => {
+    setInterval(passiveMoney, 1000)
+  }, [passiveMoney]);
+  
+  const maxHp = 1000;
+  const [money, setMoney] = useState(0);
+  const [hp, setHp] = useState(maxHp);
+
+  function handleSetHp() {
+    setHp(hp <= 0  ? maxHp : hp - 250);
+  }
+
   return (
-    <StyledFoe />
+    <>
+    <div style={{'color': 'white', 'user-select': 'none'}}>{money}</div>
+    <StyledFoe 
+      onClick={() => {
+        setMoney((newMoney) => (newMoney+10));
+        handleSetHp();
+      }}
+      maxHp={maxHp}
+      hp={hp}
+    >
+      <div className='hp-bar'></div>
+    </StyledFoe>
+    </>
   )
 }
