6d7b8b233d7d5ac43a46d0c3a38ca745298acc5f
Ann the Cat <lippastik@gmail.com>
2020-06-18 00:40:13 +0300

    Item purchase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, useCallback, useReducer } from 'react';
-import styled from 'styled-components'
-import './App.css';
-import leaf from './img/bg1.jpg';
-import StyledGlobal from './styles/globalStyles';
-import Hero from './components/Hero';
-import Foes from './components/Foes';
-import Shop from './components/Shop';
-import Money from './components/Money';
-import { basicFormReducer } from './utils/hooks'
+import React, { useState, useEffect, useCallback, useReducer } from "react";
+import styled from "styled-components";
+import "./App.css";
+import leaf from "./img/bg1.jpg";
+import StyledGlobal from "./styles/globalStyles";
+import Hero from "./components/Hero";
+import Foes from "./components/Foes";
+import Shop from "./components/Shop";
+import Money from "./components/Money";
+import { basicFormReducer } from "./utils/hooks";
 
 const StyledApp = styled.div`
   position: relative;
@@ -15,29 +15,33 @@ const StyledApp = styled.div`
   height: 100%;
   background-size: 110%;
   background-repeat: no-repeat;
-`
+`;
 
 function App() {
   const [money, setMoney] = useState(0);
-  const initialState = { moneyMult: 1 }
-  const [{moneyMult}, dispatch] = useReducer(basicFormReducer, initialState);
+  const initialState = { moneyMult: 1 };
+  const [{ moneyMult }, dispatch] = useReducer(basicFormReducer, initialState);
 
   const passiveMoney = useCallback(() => {
-    setMoney((newMoney) => (newMoney+ (1*moneyMult)))
+    setMoney((newMoney) => newMoney + 1 * moneyMult);
   }, [moneyMult]);
 
   useEffect(() => {
-    setInterval(passiveMoney, 1000)
+    setInterval(passiveMoney, 1000);
   }, [passiveMoney]);
+
+  function purchaseHandler(itemCost) {
+    setMoney((money) => (money - itemCost));
+  }
 
   return (
     <>
       <StyledGlobal />
       <StyledApp className="App">
         <Money money={money} />
-        <Shop dispatch={dispatch} moneyMult={moneyMult} />
+        <Shop dispatch={dispatch} moneyMult={moneyMult} purchaseHandler={purchaseHandler} allMoney={money} />
         <Hero />
-        <Foes handleFoeHit={() => setMoney((newMoney) => (newMoney+10))} />
+        <Foes handleFoeHit={() => setMoney((newMoney) => newMoney + 10)} />
       </StyledApp>
     </>
   );
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,107 +1,120 @@
-import React, {useState, useReducer} from 'react'
-import styled from 'styled-components';
-import ShopOpened from '../img/scroll_opened.png';
-import ShopClosed from '../img/scroll_closed.png';
-import MoneyBuff from '../img/upgrade_moneybag.png';
-import CostCoins from '../img/cost_money.png';
-import shopConfig, {shopOrder} from '../configs/shop';
+import React, { useState, useReducer } from "react";
+import styled from "styled-components";
+import ShopOpened from "../img/scroll_opened.png";
+import ShopClosed from "../img/scroll_closed.png";
+import MoneyBuff from "../img/upgrade_moneybag.png";
+import CostCoins from "../img/cost_money.png";
+import shopConfig, { shopOrder } from "../configs/shop";
 
 const StyledShop = styled.div`
-	width: 209px;
-	height: ${props => props.status ? 780 : 61}px;
-	display: block;
-	left: 20px;
-	top: 20px;
-	position: absolute;
-	background: url(${props => props.img});
-
-`
+  width: 209px;
+  height: ${(props) => (props.status ? 780 : 61)}px;
+  display: block;
+  left: 20px;
+  top: 20px;
+  position: absolute;
+  background: url(${(props) => props.img});
+`;
 
 const StyledShopItem = styled.div`
-	width: calc(100% - 90px);
-	height: 60px;
-	top: 75px;
-	left: 50px;
-	position: absolute;
-	margin-right: 10px;
-	display: flex;
-	align-items: center;
-	justify-content: flex-start;
-	flex-wrap: wrap;
-`
+  width: calc(100% - 90px);
+  height: 60px;
+  top: 75px;
+  left: 50px;
+  position: absolute;
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  cursor: pointer;
+
+  ${(props) =>
+    props.disabled &&
+    `
+		opacity: 0.5;
+		cursor: default;
+
+	`}
+`;
 
 const StyledShopItemImage = styled.div`
-	width: 60px;
-	height: 60px;
-	background-image: url(${MoneyBuff});
-	background-size: contain;
-	margin-right: 10px;
-`
+  width: 60px;
+  height: 60px;
+  background-image: url(${MoneyBuff});
+  background-size: contain;
+  margin-right: 10px;
+`;
 
 const StyledShopMultiplier = styled.div`
-	font-size: 14px;
-	color: #11110f;
-`
+  font-size: 14px;
+  color: #11110f;
+`;
 const StyledMultiplierNumber = styled.div`
-	span:first-child {
-		font-size: 15px;
-	}
-	span:last-child {
-		font-size: 30px;
-	}
-`
+  span:first-child {
+    font-size: 15px;
+  }
+  span:last-child {
+    font-size: 30px;
+  }
+`;
 const StyledShopToggler = styled.div`
-	height: 64px;
-	width: 100%;
-`
+  height: 64px;
+  width: 100%;
+`;
 
 const StyledItemCost = styled.div`
-	background-image: url(${CostCoins});
-	background-size: 30%;
-	background-repeat: no-repeat;
-	background-position: 0% 50%;
-	font-size: 25px;
-	padding-left: 30px;
-	color: #d87f0e;
-	margin-top: 5px;
-`
+  background-image: url(${CostCoins});
+  background-size: 30%;
+  background-repeat: no-repeat;
+  background-position: 0% 50%;
+  font-size: 25px;
+  padding-left: 30px;
+  color: #d87f0e;
+  margin-top: 5px;
+`;
 
 export default function Shop({
-	moneyMult,
-	dispatch,
+  moneyMult,
+  dispatch,
+  purchaseHandler,
+  allMoney,
 }) {
-	const [shopStatus, setShopStatus] = useState(false); 
+  const [shopStatus, setShopStatus] = useState(false);
+
+  function itemClickHandler(cost) {
+    if (allMoney >= cost) {
+      purchaseHandler(cost);
+      dispatch({
+        type: "changeField",
+        field: "moneyMult",
+        value: moneyMult + 1,
+      });
+    }
+  }
 
   return (
-    <StyledShop
-			status={shopStatus}
-			img={shopStatus ? ShopOpened : ShopClosed}
-    >
-			<StyledShopToggler onClick={() => setShopStatus(!shopStatus)} />
-			{ shopStatus && (
-				<StyledShopItem onClick={() => dispatch({
-					type: 'changeField',
-					field: 'moneyMult',
-					value: moneyMult + 1,
-					})}>
-					{shopOrder.map((item) => {
-						return (
-							<>
-								<StyledShopItemImage />
-								<StyledShopMultiplier>
-									<StyledMultiplierNumber>
-										<span>x</span>
-										<span>{moneyMult}</span>
-									</StyledMultiplierNumber>
-								</StyledShopMultiplier>
-								<StyledItemCost>
-									{shopConfig[item].initialCost}
-								</StyledItemCost>
-							</>
-						);
-					})}
-				</StyledShopItem>
-			) }
-		</StyledShop>
-  )
+    <StyledShop status={shopStatus} img={shopStatus ? ShopOpened : ShopClosed}>
+      <StyledShopToggler onClick={() => setShopStatus(!shopStatus)} />
+      {shopStatus &&
+        shopOrder.map((item) => {
+          return (
+            <StyledShopItem
+              key={item}
+              onClick={() => itemClickHandler(shopConfig[item].initialCost)}
+              disabled={allMoney < shopConfig[item].initialCost}
+            >
+              <StyledShopItemImage />
+              <StyledShopMultiplier>
+                <StyledMultiplierNumber>
+                  <span>x</span>
+                  <span>{moneyMult}</span>
+                </StyledMultiplierNumber>
+              </StyledShopMultiplier>
+              <StyledItemCost>{shopConfig[item].initialCost}</StyledItemCost>
+            </StyledShopItem>
+          );
+        })}
+    </StyledShop>
+  );
 }
